54270f5e7d60070dcb287f489f3c45c1769cd60a
Nebil Kriedi <nkriedi@gmail.com>
2010-12-22 02:53:21 +0100

    aggiorno le informazioni utente se è la prima volta che entro nel grafo

--- Branda/frontend/handlers/graph.py
+++ Branda/frontend/handlers/graph.py
@@ -1,7 +1,23 @@
 from base import *
+from libs.facebook import *
 
 
 class GraphHandler(BaseHandler):
     @tornado.web.authenticated
     def get(self):
-        self.render("graph.html", options = options, title = "Grafo")
+        user = self.get_current_user()
+        
+        if user.isFirstLaunch():
+            # get info from facebook
+            graph_api = GraphAPI(user.facebook_access_token)
+            user_info = graph_api.get_object("/me")
+            if not user_info:
+                raise tornado.web.HTTPError(400)
+            # update user with info
+            user.updateWithDictionary(user_info)
+            
+        self.render(
+            "graph.html", 
+            options = options, 
+            title = "Grafo",
+            needs_to_update_info = user.needsToUpdateInfo())
