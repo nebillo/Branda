d7f3ceb96e82f44c0306cad19e82972e675604ce
Nebil Kriedi <nkriedi@gmail.com>
2010-12-30 12:37:41 +0100

    invio in post i dati ricevuti da facebook

--- Branda/frontend/handlers/graph.py
+++ Branda/frontend/handlers/graph.py
@@ -1,5 +1,6 @@
 from base import *
 from libs.facebook import *
+import json
 
 
 class GraphHandler(BaseHandler):
@@ -21,3 +22,24 @@ class GraphHandler(BaseHandler):
             self.render("graph-update.html", options = options, things_to_update = things_to_update, title = "Updating")
         else:
             self.render("graph.html", options = options, title = "Grafo")
+    
+    @tornado.web.authenticated    
+    def post(self):
+        self.write("update")
+        if self.get_argument("info"):
+            info = self.get_argument("info")
+            info = json.loads(info)
+            self.write("<br>received info: " + str(len(info)))
+        if self.get_argument("likes"):
+            likes = self.get_argument("likes")
+            likes = json.loads(likes)
+            self.write("<br>received likes: " + str(len(likes)))
+        if self.get_argument("events"):
+            events = self.get_argument("events")
+            events = json.loads(events)
+            self.write("<br>received events: " + str(len(events)))
+        if self.get_argument("places"):
+            places = self.get_argument("places")
+            places = json.loads(places)
+            self.write("<br>received places: " + str(len(places)))
+            
