cbababed67a2f5971588a69eb9c0c9f8af51acf0
Nebil Kriedi <nkriedi@gmail.com>
2011-01-06 13:11:56 +0100

    effettuo l'escape dei campi letti da facebook prima di inviare il post

--- Branda/frontend/handlers/graph.py
+++ Branda/frontend/handlers/graph.py
@@ -1,6 +1,7 @@
 from base import *
 from libs.facebook import *
 from django.utils import simplejson as json
+from tornado.escape import *
 
 
 class GraphHandler(BaseHandler):
@@ -14,20 +15,25 @@ class GraphHandler(BaseHandler):
     @tornado.web.authenticated    
     def post(self):
         self.write("update")
+        user = self.get_current_user()
         if self.get_argument("info"):
             info = self.get_argument("info")
+            info = url_unescape(info)
             info = json.loads(info)
             self.write("<br>received info: " + str(len(info)))
         if self.get_argument("likes"):
             likes = self.get_argument("likes")
+            likes = url_unescape(likes)
             likes = json.loads(likes)
             self.write("<br>received likes: " + str(len(likes)))
         if self.get_argument("events"):
             events = self.get_argument("events")
+            events = url_unescape(events)
             events = json.loads(events)
             self.write("<br>received events: " + str(len(events)))
         if self.get_argument("places"):
             places = self.get_argument("places")
+            places = url_unescape(places)
             places = json.loads(places)
             self.write("<br>received places: " + str(len(places)))
             
