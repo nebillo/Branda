c7395eebdc21d5daf45e9b5d5c1de9372b939c59
Nebil Kriedi <nkriedi@gmail.com>
2011-01-08 14:00:13 +0100

    corretto handler di aggiornamento grafo

--- Branda/frontend/handlers/graph.py
+++ Branda/frontend/handlers/graph.py
@@ -1,8 +1,8 @@
 from base import *
 from libs.facebook import *
-from django.utils import simplejson as json
 from tornado.escape import *
 import datetime
+import logging
 
 
 class GraphHandler(BaseHandler):
@@ -23,27 +23,30 @@ class GraphDataHandler(BaseHandler):
     def post(self):
         self.write("update data")
         user = self.get_current_user()
+        updated = False
         
         if self.get_argument("info"):
             info = self.get_argument("info")
-            info = url_unescape(info)
-            info = json.loads(info)
-            self.write("<br>received info: " + str(len(info)))
+            info = json_decode(info)
+            logging.info("received info: " + str(len(info)))
+            updated = True
         if self.get_argument("likes"):
             likes = self.get_argument("likes")
-            likes = url_unescape(likes)
-            likes = json.loads(likes)
-            self.write("<br>received likes: " + str(len(likes)))
+            likes = json_decode(likes)
+            logging.info("received likes: " + str(len(likes)))
+            updated = True
         if self.get_argument("events"):
             events = self.get_argument("events")
-            events = url_unescape(events)
-            events = json.loads(events)
-            self.write("<br>received events: " + str(len(events)))
+            events = json_decode(events)
+            logging.info("received events: " + str(len(events)))
+            updated = True
         if self.get_argument("places"):
             places = self.get_argument("places")
-            places = url_unescape(places)
-            places = json.loads(places)
-            self.write("<br>received places: " + str(len(places)))
+            places = json_decode(places)
+            logging.info("received places: " + str(len(places)))
+            updated = True
+        
+        self.write(json_encode({"updated": updated}))
         
         user.updated_at = datetime.datetime.now()
         user.put()
