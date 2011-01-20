e4b4b1e84a239860b77e00a79f411d379b41898a
Nebil Kriedi <nkriedi@gmail.com>
2011-01-20 23:49:52 +0100

    creato test per checkup globale

---
+++ Branda/test/checkup.py
@@ -0,0 +1,22 @@
+import unittest
+
+
+class Checkup(unittest.TestCase):
+    def test_true(self):
+        self.assertTrue(True)
+        
+    def test_venue(self):
+        from model.venue import Venue
+        self.assertTrue(True)
+
+    def test_thing(self):
+        from model.thing import Thing
+        self.assertTrue(True)
+        
+    def test_user(self):
+        from model.user import User
+        self.assertTrue(True)
+        
+    def test_affinity(self):
+        from model.affinity import Affinity, UserLinking, VenueLinking
+        self.assertTrue(True)
